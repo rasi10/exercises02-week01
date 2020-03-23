22341bb4d9538ff9f103955168df404493bbeaae
Rafael Silva <rasilva.1986@gmail.com>
2020-03-23 20:45:15 +0100

    Working on exercise 08

---
+++ src/Exercise08.java
@@ -0,0 +1,15 @@
+
+public class Exercise08 {
+	public static void main(String[] args) {
+		for (int i = 0; i < 21; i++) {
+			if (i % 2 == 0) {
+				System.out.println("The number: "+ i + " is even");
+			}else {
+				System.out.println("The number: "+ i + " is odd");
+			}
+			
+		}
+
+	}
+
+}
